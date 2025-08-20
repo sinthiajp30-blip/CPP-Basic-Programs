707e0e757aed0884f8b8483c3c18dfd522c43d4e
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-08-20 23:37:01 +0600

    6floatingnumber

---
+++ 6floatingnumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x, y, result;
+
+    cout << "Enter the first floating-point number: ";
+    cin >> x;
+
+    cout << "Enter the second floating-point number: ";
+    cin >> y;
+
+    result = x * y;
+
+    cout << "result: " << result << endl;
+
+    return 0;
+}
+
