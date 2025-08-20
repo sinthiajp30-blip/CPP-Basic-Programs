050519747f1e000e4d684f3d4d78e34daa676122
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-08-20 23:06:22 +0600

    addtwonumbers

---
+++ addtwonumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, sum;
+
+    cout << "Enter the first number: ";
+    cin >> x;
+
+    cout << "Enter the second number: ";
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
+
