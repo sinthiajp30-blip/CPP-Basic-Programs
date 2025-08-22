cb8349ceba24b71534ed4c67ad1920fe58d0fcee
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-08-23 01:56:04 +0600

    21

---
+++ 21.c
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Alphabets from A to Z:" << endl;
+
+    for (ch = 'A'; ch <= 'Z'; ++ch) {
+        cout << ch << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
---
+++ 21.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+
+    cout << "Alphabets from A to Z:" << endl;
+
+    for (c = 'A'; c <= 'Z'; ++c) {
+        cout << c << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
+
