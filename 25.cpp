2fbba1b510965050625dc1a451d088b838ad041a
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-08-23 12:45:20 +0600

    25

---
+++ 25.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter a number to generate its multiplication table: ";
+    cin >> n;
+
+    cout << "Multiplication Table of " << n << ":" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
+
