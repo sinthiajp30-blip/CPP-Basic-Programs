8a67b903595da2e7cc3590c12ef520429686764e
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-25 00:31:13 +0600

    32

---
+++ 32.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, original, reversed = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    if (original == reversed)
+        cout << original << " is a palindrome number." << endl;
+    else
+        cout << original << " is not a palindrome number." << endl;
+
+    return 0;
+}
+
