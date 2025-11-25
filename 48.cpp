807f0282b03f1f1a96b60647521ed6c14ce2360a
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-25 22:33:07 +0600

    48

---
+++ 48.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows (half-diamond height): ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int s = 0; s < n - i; s++)
+            cout << " ";
+        for (int j = 1; j <= 2*i - 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 0; s < n - i; s++)
+            cout << " ";
+        for (int j = 1; j <= 2*i - 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
+
