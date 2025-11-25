453f60f79957625c104bb19529d958e1dbf5dd98
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-25 22:06:02 +0600

    41

---
+++ 41.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
