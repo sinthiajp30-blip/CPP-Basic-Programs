84b7263128f5d039ba9f828fd7ca0e554aeac2c8
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-26 00:17:30 +0600

    61

---
+++ 61.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[rows][cols];
+    cout << "Enter elements of the 2D array:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    cout << "The 2D array is:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
