f3df02cc48a94b0458d7c738162d02c1b4150f08
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-26 00:21:44 +0600

    62

---
+++ 62.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    cout << "The largest element in the array is: " << largest << endl;
+
+    return 0;
+}
+
