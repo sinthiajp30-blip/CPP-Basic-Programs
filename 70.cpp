6d98cdde69d9afd92f5e9686925c8934457f2f0d
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 00:49:56 +0600

    70

--- 70.cpp
+++ 70.cpp
@@ -3,28 +3,26 @@ using namespace std;
 
 int main() {
     int n;
-
     cout << "Enter the number of elements: ";
     cin >> n;
 
     int arr[n];
-
     cout << "Enter " << n << " elements:\n";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-        arr[j + 1] = key;
     }
 
-    cout << "Sorted array: ";
+    cout << "Array in Descending Order: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
