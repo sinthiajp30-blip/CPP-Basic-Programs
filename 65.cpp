7341e2c652b9f0f12145a76f4f9502e7988ab35e
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 00:24:36 +0600

    65

---
+++ 65.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+
+    cout << "Average of the array elements = " << average << endl;
+
+    return 0;
+}
+
