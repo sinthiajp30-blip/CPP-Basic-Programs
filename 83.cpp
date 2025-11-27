f53557f83702e8d73d73eba7ec022fb453db31fd
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 01:44:29 +0600

    83

---
+++ 83.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    int mat1[rows][cols], mat2[rows][cols], sum[rows][cols];
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            cin >> mat1[i][j];
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            cin >> mat2[i][j];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+    cout << "Sum of the matrices:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
