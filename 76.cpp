896d447b561c26e423c1e4542136dc6d4e057baf
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 01:19:33 +0600

    76

---
+++ 76.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr1[n], arr2[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+    for (int i = 0; i < n; i++) {
+        arr2[i] = arr1[i];
+    }
+    cout << "Elements of the second array (copied): ";
+    for (int i = 0; i < n; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
