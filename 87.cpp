dd8e444fa156b6e10f230064726fc7b5d2414890
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 01:59:30 +0600

    87

---
+++ 87.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the square matrix: ";
+    cin >> n;
+
+    int matrix[n][n];
+
+
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int mainDiagonalSum = 0;
+    int secondaryDiagonalSum = 0;
+    for (int i = 0; i < n; i++) {
+        mainDiagonalSum += matrix[i][i];
+        secondaryDiagonalSum += matrix[i][n - i - 1];
+    }
+
+    cout << "Sum of main diagonal: " << mainDiagonalSum << endl;
+    cout << "Sum of secondary diagonal: " << secondaryDiagonalSum << endl;
+
+    return 0;
+}
+
