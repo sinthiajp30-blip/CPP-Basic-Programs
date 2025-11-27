20327e1d1dae93bac29e68576d28d92b5baf7644
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 02:27:20 +0600

    95

--- 95.cpp
+++ 95.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseString(string &str, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    swap(str[start], str[end]);
+    reverseString(str, start + 1, end - 1);
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    reverseString(str, 0, str.length() - 1);
+
+    cout << "Reversed string: " << str << endl;
+
+    return 0;
+}
