3a584163b6ef6be9561f5742ac0970a4c7f56717
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 01:25:55 +0600

    78

---
+++ 78.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[rows][cols];
+
+    cout << "Enter elements of the 2D array:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        sort(arr[i], arr[i] + cols);
+    }
+
+    cout << "2D array after sorting each row:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
