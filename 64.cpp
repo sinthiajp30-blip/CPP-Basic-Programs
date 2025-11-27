edae25b2d37c1677049c352e4d31b622da56ebf2
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 00:18:43 +0600

    64

---
+++ 64.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " sorted elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+    bool found = false;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            cout << "Element found at index " << mid << endl;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        cout << "Element not found in the array." << endl;
+    }
+
+    return 0;
+}
+
