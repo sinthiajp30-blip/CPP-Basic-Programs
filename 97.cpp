4321ac88fe3530c637ea5747cae57a8ffc90dc5c
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 11:53:43 +0600

    97

---
+++ 97.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    sort(str.begin(), str.end());
+
+    cout << "Sorted string: " << str << endl;
+
+    return 0;
+}
+
