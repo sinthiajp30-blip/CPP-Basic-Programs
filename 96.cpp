f753c433e74ac58e0159e16bbedbb2f03bbc069e
sinthiajp30-blip <sinthiajp30@gmail.com>
2025-11-28 11:51:37 +0600

    96

---
+++ 96.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Length of the string: " << str.length() << endl;
+
+    return 0;
+}
+
